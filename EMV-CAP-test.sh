6678c84403645433c5521eaf903f1ff3441e8186
Philippe Teuwen <phil@teuwen.org>
2012-04-28 23:42:26 +0200

    Adding setup.py to facilitate distribution & installation

--- EMV-CAP-test.sh
+++ EMV-CAP-test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-function EMV-CAP {
+function validate {
     # $1: CARD $2: OTP $3:MODE $4: DATA (optional)
     echo "TEST $1 $3 $4"
-    ./EMV-CAP.py -$3 $4 -r foo:$1 |\
+    ./EMV-CAP -$3 $4 -r foo:$1 |\
       grep ": $2\$" || echo "****** FAIL ******"
 }
 
-EMV-CAP cap_be           23790240 m1
-EMV-CAP cap_be           23580039 m1 1234
-EMV-CAP cap_be           23719593 m2 1234
-EMV-CAP maestro_be       53780079 m1
-EMV-CAP visa_dpa_be      19814125 m1
-EMV-CAP visa_dpa_fr      34656023 m1
-EMV-CAP visa_cleo_fr    102823328 m1
-EMV-CAP cap_abnamro_nl   34998891 m1 24661140
-EMV-CAP cap_fc09_uk       4822527 m1 12345678
-EMV-CAP bancontact_be    53780079 m1
-EMV-CAP eid_pt            8669448 m1
-EMV-CAP visa_be          53780079 m1
-EMV-CAP cap_rabo1_nl     07986951 m1
-EMV-CAP cap_rabo2_nl     08180460 m2 0530026806
-EMV-CAP maestro_lu       75176930639874 m1
-# ./EMV-CAP.py -L -r foo:pse_uk
-# ./EMV-CAP.py -m1 -r foo:visa_rosa_sk -v -d
+validate cap_be           23790240 m1
+validate cap_be           23580039 m1 1234
+validate cap_be           23719593 m2 1234
+validate maestro_be       53780079 m1
+validate visa_dpa_be      19814125 m1
+validate visa_dpa_fr      34656023 m1
+validate visa_cleo_fr    102823328 m1
+validate cap_abnamro_nl   34998891 m1 24661140
+validate cap_fc09_uk       4822527 m1 12345678
+validate bancontact_be    53780079 m1
+validate eid_pt            8669448 m1
+validate visa_be          53780079 m1
+validate cap_rabo1_nl     07986951 m1
+validate cap_rabo2_nl     08180460 m2 0530026806
+validate maestro_lu       75176930639874 m1
+# EMV-CAP -L -r foo:pse_uk
+# EMV-CAP -m1 -r foo:visa_rosa_sk -v -d
---
+++ setup.py
@@ -0,0 +1,25 @@
+# Standard setup.py, cf http://docs.python.org/distutils/index.html
+#
+# Usages:
+#
+# To prepare a release:
+#   python setup.py sdist
+#
+# To install a release:
+#   tar xzf EMVCAP-X.Y.tar.gz
+#   cd EMVCAP-X.Y
+#   python setup.py install
+
+from distutils.core import setup
+setup(name='EMVCAP',
+      version='1.2',
+      description='EMV-CAP device emulation script',
+      long_description='This tool emulates an EMV-CAP device, to illustrate the article "Banque en ligne : a la decouverte d\'EMV-CAP" published in MISC, issue #56 and available online at http://www.unixgarden.com/index.php/misc/banques-en-ligne-a-la-decouverte-demv-cap',
+      author='Philippe Teuwen & Jean-Pierre Szikora',
+      author_email='phil@teuwen.org',
+      url='http://sites.uclouvain.be/EMV-CAP/',
+      license='Contrat Attribution-NoDerivs 2.0 Belgium http://creativecommons.org/licenses/by-nd/2.0/be/',
+      py_modules=['EMVCAPcore', 'EMVCAPfoo'],
+      scripts=['EMV-CAP'],
+      requires=['argparse', 'pycrypto', 'pyscard']
+      )
