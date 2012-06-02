a4c30d555bab0652d548c61bbfbaef0976d26bb9
Philippe Teuwen <phil@teuwen.org>
2012-06-02 21:25:00 +0200

    Add IV to DES.new(), mandatory since pycrypto v2.6

--- EMVCAPcore.py
+++ EMVCAPcore.py
@@ -554,7 +554,7 @@ def generate_otp_be(atc, ac, debug=False):
 
 
 def mix_tds(ac, mdata, debug=False):
-    des = DES.new(key=ac.decode('hex'), mode=DES.MODE_CBC)
+    des = DES.new(key=ac.decode('hex'), mode=DES.MODE_CBC, IV='\x00'*8)
     data = 'F'.join([str(i) for i in mdata])
     if len(data) % 2:
         data += 'F'
